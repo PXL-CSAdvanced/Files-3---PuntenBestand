5027b394760da1041c055e05c0edd554fdd7eda3
Koen Bloemen <koen.bloemen@pxl.be>
2025-03-11 19:18:04 +0100

    final

--- Files-3---PuntenBestand/MainWindow.xaml.cs
+++ Files-3---PuntenBestand/MainWindow.xaml.cs
@@ -18,7 +18,18 @@ namespace Files_3___PuntenBestand;
 /// </summary>
 public partial class MainWindow : Window
 {
-    private Dictionary<string, float> _resultList = new Dictionary<string, float>();
+    // Dialoogvenster op voorhand aanmaken (gebruiken we in meerdere event procedures)
+    private OpenFileDialog ofd = new OpenFileDialog()
+    {
+        Filter = "Csv bestanden (*.csv) |*.csv|Tekstbestanden (*.txt) |*.txt|Alle bestanden (*.*)|*.*",
+        InitialDirectory = System.IO.Path.GetFullPath(@"..\..\Bestanden"),
+        Title = "Bestand openen",
+        FileName = "Punten.csv",
+        AddExtension = true,
+        DefaultExt = "csv"
+    };
+
+    private List<PuntenAdmin> pntAdmin = new List<PuntenAdmin>();
 
     public MainWindow()
     {
@@ -27,121 +38,110 @@ public partial class MainWindow : Window
 
     private void readButton_Click(object sender, RoutedEventArgs e)
     {
-        OpenFileDialog ofd = new OpenFileDialog()
+        StringBuilder sb = new StringBuilder();
+        if (ofd.ShowDialog() != true)
         {
-            Filter = "Alle bestanden|*.*|Tekstbestanden|*.txt",
-            FilterIndex = 2,
-            InitialDirectory = Environment.CurrentDirectory, // onder bin\Debug
-            Title = "Bestand openen",
-            AddExtension = true,
-            DefaultExt = "txt",
-            FileName = "Punten.txt"
-        };
-
-        if (ofd.ShowDialog() == false)
-        {
-            return; // Indien we niet op Openen klikken
+            MessageBox.Show("File does not exist",
+                    "Error message",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Warning);
+            return;
         }
 
-        StringBuilder sb = new StringBuilder();
-        using (StreamReader sr = new StreamReader(ofd.FileName))
+        string fileName = ofd.FileName;
+        FileInfo fi = new FileInfo(fileName);
+        if (!fi.Exists)
+        {
+            return;
+        }
+
+        using (StreamReader sr = fi.OpenText())
         {
             while (!sr.EndOfStream)
             {
-                string line = sr.ReadLine();
-                string name = line.Substring(0, 18).Trim();
-                string mail = line.Substring(19, 20).Trim();
-                string gender = line.Substring(40, 1); // Geen Trim() nodig, want 1 karakter maar
-                string pointsNotation = line.Substring(48, 6).Trim();
-                sb.AppendLine($"{name,-21}{mail,-21}{gender,-5}{pointsNotation,-9}");
+                PuntenAdmin ad = new PuntenAdmin();
+                string[] lines = sr.ReadLine().Split(';'); // CSV-file
+                string[] name = lines[0].Trim().Split(' ');
+                ad.Familyname = name[0].Trim();
+                ad.Firstname = name[1].Trim();
+                ad.Email = lines[1].Trim();
+                ad.Gender = lines[2].Trim();
 
-                float point = float.Parse(pointsNotation.Substring(0, 3)); // eerste 3 cijfers
-                float max = float.Parse(pointsNotation.Substring(3, 3)); // laatste 3 cijfers
-                float points = point / max; // kommagetal van 0 tot 1
-                _resultList[name] = points; // puntenlijst.Add(naam, punten);
+                string score = lines[3].Substring(0, 3).Trim();
+                string total = lines[3].Substring(3, 3).Trim();
+                ad.Score = int.Parse(score);
+                ad.TotalScore = int.Parse(total);
+
+                sb.Append($"{(ad.Firstname + ' ' + ad.Familyname),-21}{ad.Email,-21}");
+                sb.Append($"{ad.Gender,-5}{ad.Score}/{ad.TotalScore}".Replace("\"", ""));
+                sb.AppendLine();
+
+                pntAdmin.Add(ad);
             }
         }
-        // De TextBox gaat enkel en alleen mooie uitgelijnde kolommen hebben
-        // wanneer we de FontFamily instellen op een monospaced lettertype!!! vb/ Consolas
-        // (FontFamily="Consolas")
-        resultTextBox.Text = sb.ToString();
         transformButton.IsEnabled = true;
+        resultTextBox.Text = sb.ToString();
     }
 
     private void transformButton_Click(object sender, RoutedEventArgs e)
     {
-        SaveFileDialog sfd = new SaveFileDialog()
+        SaveFileDialog sfd = new SaveFileDialog
         {
-            Filter = "Alle bestanden (*.*)|*.*|Tekstbestanden (*.txt)|*.txt", // keuze uit deze bestandstypes
-            FilterIndex = 2, // kies het 2de (tekstbestanden) uit bovenstaande filter standaard aangeduid
-            Title = "Geef een bestandsnaam op", // titel
-            OverwritePrompt = true,  // bevestiging vragen bij overschrijven van bestand 
-            AddExtension = true, // extensie wordt toegevoegd aan bestand
-            DefaultExt = "txt", // standaardextensie die wordt toegevoegd is txt
-            FileName = "PuntenVerwerkt.txt", // bestandsnaam van bestand dat je wil opslaan
-            InitialDirectory = Environment.CurrentDirectory // onder bin\Debug
+            Filter = "Alle bestanden (*.*)|*.*| CSV bestanden (*.csv)|*.csv| Tekstbestanden (*.txt)|*.txt",
+            FilterIndex = 2,
+            InitialDirectory = System.IO.Path.GetFullPath(@"..\..\Bestanden"),
+            Title = "Geef een bestandsnaam op",
+            FileName = "PuntenVerwerkt.csv",
+            // Bevestiging vragen bij overschrijven van een bestand
+            OverwritePrompt = true
         };
 
-        if (sfd.ShowDialog() == false)
+        if (sfd.ShowDialog() != true)
         {
-            return; // Indien we niet op Opslaan klikken
+            MessageBox.Show("De resultaten zijn niet verwerkt.",
+                "Resultaten",
+                MessageBoxButton.OK,
+                MessageBoxImage.Information);
+            return;
         }
 
-        using (StreamWriter sw = new StreamWriter(sfd.FileName))
+        // Bestand opslaan
+        string fileName = sfd.FileName;
+        using (StreamWriter sw = File.CreateText(fileName))
         {
-            foreach (var paar in _resultList)
+            // Gegevens wegschrijven
+            foreach (var item in pntAdmin)
             {
-                string name = paar.Key;
-                float points = paar.Value;
-                string grade = (points >= 0.85f) ? "Geslaagd" : "Niet Geslaagd";
-
-                // :P zet kommagetal tussen 0 en 1 om naar percentage met 2 cijfers achter de komma
-                sw.WriteLine($"{name,-24}{points,-10:P}{grade,-20}");
+                sw.WriteLine($"{item.Familyname + ' ' + item.Firstname,-50}{item.Percent(),-10:p}{item.Grade(),-10}");
             }
         }
-        checkButton.IsEnabled = true;
 
-        MessageBox.Show($"De resultaten zijn verwerkt. Je kan de resultaten nalezen in het bestand {sfd.FileName}",
+        MessageBox.Show($"De resultaten zijn verwerkt. Je kan de resultaten nalezen in het bestand {fileName}",
             "Resultaten",
             MessageBoxButton.OK,
             MessageBoxImage.Information);
+        checkButton.IsEnabled = true;
     }
 
     private void checkButton_Click(object sender, RoutedEventArgs e)
     {
-        OpenFileDialog ofd = new OpenFileDialog()
-        {
-            Filter = "Alle bestanden (*.*)|*.*|Tekstbestanden (*.txt)|*.txt",
-            FilterIndex = 2,
-            InitialDirectory = Environment.CurrentDirectory,
-            Title = "Bestand openen",
-            AddExtension = true,
-            DefaultExt = "txt",
-            FileName = "PuntenVerwerkt.txt"
-        };
+        ofd.ShowDialog();
+        string fileName = ofd.FileName;
 
-        resultTextBox.Clear();
-        if (ofd.ShowDialog() == false)
+        FileInfo fi = new FileInfo(fileName);
+        if (!fi.Exists)
         {
-            return; // Indien we niet op Openen klikken
-        }
-
-        FileInfo fi = new FileInfo(ofd.FileName);
-        if (fi.Exists)
-        {
-            string text;
-            using (StreamReader sr = fi.OpenText())
-            {
-                text = sr.ReadToEnd();
-            }
-            resultTextBox.Text = text;
-        }
-        else
-        {
-            MessageBox.Show($"Bestand {System.IO.Path.GetFileName(ofd.FileName)} bestaat niet",
+            resultTextBox.Clear();
+            MessageBox.Show($"Bestand {System.IO.Path.GetFileName(fileName)} bestaat niet",
                 "Foutmelding",
                 MessageBoxButton.OK,
-                MessageBoxImage.Information);
+                MessageBoxImage.Error);
+            return;
+        }
+
+        using (StreamReader sr = fi.OpenText())
+        {
+            resultTextBox.Text = sr.ReadToEnd();
         }
     }
 
---
+++ Files-3---PuntenBestand/PuntenAdmin.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Files_3___PuntenBestand
+{
+    public class PuntenAdmin
+    {
+        private float result = 0.0f;
+        // ===  Auto-implemented properties  ===  (snippet: prop)
+        public string Firstname { get; set; }
+        public string Familyname { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public int Score { get; set; }      // punten
+        public int TotalScore { get; set; } // puntentotaal
+
+        // === Methods ===
+        public float Percent() // behaald percentage  -> vb. 75% = 0.75f
+        {
+            result = (float)Score / TotalScore;
+            return result;
+        }
+
+        public string Grade() // beoordeling -> geslaagd / niet geslaagd
+        {
+            return (Percent() >= 0.8) ? "Geslaagd" : "Niet Geslaagd";
+        }
+
+        //  === Constructors  === (snippet: ctor)
+        public PuntenAdmin() : this("", "", "", "onzijdig", 10, 20)
+        {
+        }
+
+        public PuntenAdmin(string firstname, string familyname, string email,
+            string gender, int score, int totalScore)
+        {
+            Firstname = firstname;
+            Familyname = familyname;
+            Email = email;
+            Gender = gender;
+            Score = score;
+            TotalScore = totalScore;
+        }
+
+    }
+}
